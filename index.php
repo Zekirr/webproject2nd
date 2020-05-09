eed1b205dda3cd5d4b56a93a95088c4757f3aa69
Zekirr <gianlucaltz@gmail.com>
2020-05-09 21:01:34 +0200

    landingpage front v1 + scrollbar v1

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Noxus Stats</title>
+    <script src="https://kit.fontawesome.com/4a87f9d18c.js" crossorigin="anonymous"></script>  <!-- intégration kit d'icônes -->
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="scrollbar.css">
+</head>
+
+<body>
+
+<img id="logo" src="ressources/Noxus_logo.png">
+
+<div class="pagecontent">
+    <p>Découvrez les stats de vos différentes parties</p>
+</div>
+
+<form class="searchbar">
+    <input type="search" placeholder="Summoner name.." id="searchinput">
+    <button type="submit" id="searchbutton">
+        <i class="fas fa-search"></i>
+    </button>
+</form>
+
+
+<div class="backgroundcontainer">
+    <img id="backgroundimage" src="ressources/noxus_splash.jpg">
+</div>
+
+</body>
