c50650acd2ae1b3c9ae0a5d004250d0f64d57c1c
Zekirr <gianlucaltz@gmail.com>
2020-05-10 16:46:07 +0200

    header front v1

---
+++ header.php
@@ -0,0 +1,25 @@
+<?php ?>
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+<header>
+    <div class="logo">
+    <a href="index.php"><img id="logo" src="ressources/Noxus_logo.png" alt="logo"></a>
+    </div>
+    <nav>
+        <ul class="nav_links">
+            <li><a href="#">Challengers</a></li>
+            <li><a href="#">Rotation des champions</a></li>
+        </ul>
+    </nav>
+    <div class="dropdown">
+        <button class="boutonprincipal">Region</button>
+        <div class="dropdown-child">
+            <a href="#">EUW</a>
+            <a href="#">EUNE</a>
+            <a href="#">NA</a>
+            <a href="#">KR</a>
+        </div>
+    </div>
