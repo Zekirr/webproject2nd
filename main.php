dc6af095f2d18b5ee3cfaeadc6f3ac472dd9f32d
Zekirr <gianlucaltz@gmail.com>
2020-05-11 15:48:16 +0200

    ajout de main front v1

---
+++ main.php
@@ -0,0 +1,39 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Noxus Stats</title>
+    <script src="https://kit.fontawesome.com/4a87f9d18c.js" crossorigin="anonymous"></script>  <!-- intégration kit d'icônes -->
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="scrollbar.css">
+    <link rel="stylesheet" href="header.css">
+
+</head>
+
+<?php include_once "header.php"?>
+
+<div class="summoner_info">
+
+    <img id="avatar" src="ressources/pp.jpg">
+    <span id="summonername">Bruhmaster</span>
+
+</div>
+
+<img id="rank" src="ressources/diams.png">
+
+<table>
+    <thead>
+        <tr>
+            <td>Invocateur</td>
+            <td>Winrate</td>
+            <td>Points</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><img src="ressources/pp.jpg"> test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+    </tbody>
+</table>
