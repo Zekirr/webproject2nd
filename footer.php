788c2deb50b01a9a78efff50b322904fc51fb226
emmanuel-storm <61197043+emmanuel-storm@users.noreply.github.com>
2020-05-12 10:55:05 +0200

    ajout du footer

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <title>footer</title>
+            <link rel="stylesheet" href="footer.css">
+        </head>
+        <body>
+            <div id="main">
+                <footer>
+                    <span>Blackstar.69 | Zekir | Psycho_V9 | Rami J.31 | © 2020 Noxus Stats, Inc.</span>
+                    <p>Noxus Stats, rapidité et éfficacité</p>
+                </footer>
+            </div>
+        </body>
+    </html>
+
+
+
+
+
