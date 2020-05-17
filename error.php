2a2c4cc0cfd4046be7f6d4fc4160138f36f45bd9
ramijohnstone <johnstone.rami@univ-catholyon.fr>
2020-05-17 18:01:57 +0200

    sauge

---
+++ error.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="error.css"/>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+<div class="content">
+    <div class="header">
+        <h2>ERREUR</h2>
+        <label  class="fas fa-times"></label>
+    </div>
+    <i class="fas fa-exclamation-triangle"></i>
+    <p>TEXT</p>
+    <div class="line"></div>
+    <label  class="close-btn">Fermer</label>
+</div>
+
+
+
+</body>
+</html>
